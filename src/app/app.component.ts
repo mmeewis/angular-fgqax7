55cf8ee3ffd0ec69609cfa233f7c60d5577124ca
mmeewis <marc.meewis@gmail.com>
2021-08-18 15:00:26 +0200

    Added router to capture target

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 
+import {
+  Router,
+  NavigationStart,
+  NavigationEnd,
+  RoutesRecognized
+} from '@angular/router';
+
+declare const targetView: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-}
 
+export class AppComponent {
+  constructor(private router: Router) {
+    router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        targetView();
+      }
+    });
+  }
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,8 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 
+
+
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
